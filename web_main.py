572004d8970c80c64d7cc72cfd793ed4a7958246
vhuang1 <huangsx0213@163.com>
2024-07-06 21:42:56 +0800

    refactor

--- web_main.py
+++ web_main.py
@@ -1,18 +1,25 @@
-from libraries.web.excel_reader import ExcelReader
-from libraries.web.test_executor import TestExecutor
-from libraries.web.webdriver_factory import WebDriverFactory
+import os
 
+from libraries.common.config_manager import ConfigManager
+from libraries.e2e.e2e_test_executor import E2ETestExecutor
+from libraries.web.excel_reader import ExcelReader
+from libraries.web.webdriver_factory import WebDriverFactory
+from libraries.common.utility_helpers import UtilityHelpers, PROJECT_ROOT
 
 def main():
-    excel_path = 'test_cases/web_test_cases.xlsx'
-    chrome_path = r"E:\Downloads\my files\chrome-win64\chrome.exe"
-    chromedriver_path = r"E:\Downloads\my files\chromedriver-win64\chromedriver.exe"
+    project_root: str = PROJECT_ROOT
+    config_path = os.path.join(project_root,  'configs', 'web', 'web_config.yaml')
+    test_config = ConfigManager.load_yaml(config_path)
 
-    excel_data_manager = ExcelReader(excel_path)
+    chrome_path = test_config.get("chrome_path", None)
+    chromedriver_path = test_config.get("chromedriver_path", None)
+    test_case_path = test_config.get("test_case_path", None)
+
+    excel_data_manager = ExcelReader(test_case_path)
     driver = WebDriverFactory.create_chrome_driver(chrome_path, chromedriver_path)
 
     try:
-        test_executor = TestExecutor(driver, excel_data_manager)
+        test_executor = E2ETestExecutor(driver, excel_data_manager)
         test_executor.run_tests()
     finally:
         WebDriverFactory.quit_driver(driver)
