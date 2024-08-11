dc4c2778614cbf3e4befb1e79504b2c87cc1511d
huangsx0213 <huangsx0213@163.com>
2024-08-11 09:54:05 +0800

    fix error handling

--- libraries/api/request_sender.py
+++ libraries/api/request_sender.py
@@ -22,7 +22,7 @@ class RequestSender:
                 raise ValueError(f"RequestSender: Unsupported format type: {format_type}")
             response.raise_for_status()
         except (requests.RequestException, ValueError) as e:
-            raise ValueError(f"RequestSender: sending request error: {str(e)}\nwith response text:\n{response.text}")
+            raise ValueError(f"RequestSender: sending request error: {str(e)}")
 
         execution_time = time.time() - start_time
         return response, execution_time
